3c19e1e2eac40caa367198acb2d693237dc483ed
Sean Murphy <C00207873@itcarlow.ie>
2018-01-25 00:12:10 +0000

    added functionality

--- Converter/Form1.Designer.cs
+++ Converter/Form1.Designer.cs
@@ -28,72 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.Windows.Forms.ComboBox comboBox1;
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // comboBox1
-            // 
-            comboBox1.AutoCompleteCustomSource.AddRange(new string[] {
-            "Inches to Feet",
-            "Pounds to Euro",
-            "Cel to Fahr"});
-            comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Items.AddRange(new object[] {
-            "Inches to Feet",
-            "Pounds to Euro",
-            "Cel to Fahr"});
-            comboBox1.Location = new System.Drawing.Point(17, 16);
-            comboBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new System.Drawing.Size(160, 24);
-            comboBox1.TabIndex = 1;
-            comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(211, 107);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button1.Location = new System.Drawing.Point(158, 87);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 53);
+            this.button1.Size = new System.Drawing.Size(75, 43);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Convert";
+            this.button1.Text = "Pound to Euro";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(17, 107);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox1.Location = new System.Drawing.Point(13, 87);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(159, 53);
+            this.textBox1.Size = new System.Drawing.Size(120, 44);
             this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(361, 107);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox2.Location = new System.Drawing.Point(271, 87);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(150, 53);
+            this.textBox2.Size = new System.Drawing.Size(114, 44);
             this.textBox2.TabIndex = 3;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(158, 136);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 43);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Cel to Fahr";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(158, 38);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 43);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Feet to Inches";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
-            this.ClientSize = new System.Drawing.Size(535, 278);
+            this.ClientSize = new System.Drawing.Size(401, 226);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Controls.Add(comboBox1);
             this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Converter";
             this.ResumeLayout(false);
@@ -106,6 +107,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- Converter/Form1.cs
+++ Converter/Form1.cs
@@ -12,6 +12,10 @@ namespace Converter
 {
     public partial class Form1 : Form
     {
+        string option = string.Empty;
+        double inOne = 0;
+        double result;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,12 +23,42 @@ namespace Converter
 
         private void button1_Click(object sender, EventArgs e)
         {
+            double.TryParse(textBox1.Text, out inOne);
+            result = inOne / .79 ;
+            textBox2.Text = result.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double.TryParse(textBox1.Text, out inOne);
+            result = (inOne * 1.8) + 32;
+            textBox2.Text = result.ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double.TryParse(textBox1.Text, out inOne);
+            result = inOne / 12;
+            textBox2.Text = result.ToString();
 
         }
 
-        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        private void textBox2_TextChanged(object sender, EventArgs e)
         {
+            
+        }
 
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
         }
     }
 }
